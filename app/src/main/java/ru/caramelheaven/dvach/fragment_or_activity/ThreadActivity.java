b4497d48de77e000d5d63d649534aa44395c612b
CaramelHeaven <srg.fominov@gmail.com>
2017-12-28 19:20:08 +0400

    find the better clickListener

--- app/src/main/java/ru/caramelheaven/dvach/fragment_or_activity/ThreadActivity.java
+++ app/src/main/java/ru/caramelheaven/dvach/fragment_or_activity/ThreadActivity.java
@@ -6,22 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import ru.caramelheaven.dvach.R;
-import ru.caramelheaven.dvach.ThreadAdapter;
+import ru.caramelheaven.dvach.adapters.ThreadAdapter;
 import ru.caramelheaven.dvach.data.Board;
 import ru.caramelheaven.dvach.data.Post;
 import ru.caramelheaven.dvach.network.DvachService;
 
-import static ru.caramelheaven.dvach.BoardAdapter.NUMBER_THREAD;
+import static ru.caramelheaven.dvach.MainActivity.GET_BOARD;
+import static ru.caramelheaven.dvach.adapters.BoardAdapter.NUMBER_THREAD;
 
 public class ThreadActivity extends AppCompatActivity {
 
@@ -36,20 +36,48 @@ public class ThreadActivity extends AppCompatActivity {
 
         recyclerViewq = findViewById(R.id.recycle_view_thread);
 
-        Retrofit.Builder builder = new Retrofit.Builder()
-                .baseUrl("https://2ch.hk")
-                .addConverterFactory(GsonConverterFactory.create());
-
-        Retrofit retrofit = builder.build();
-
-        DvachService dvachService = retrofit.create(DvachService.class);
-        Call<Board> call = dvachService.getThread("b", f); // Здесь должен быть GET_BOARD, но он не работает.
         List<Post> posts = new ArrayList<>();
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerViewq.setHasFixedSize(true);
         recyclerViewq.setLayoutManager(layoutManager);
+        
+        /*Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://2ch.hk")
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit = builder.build();
+        DvachService dvachService = retrofit.create(DvachService.class);*/
 
+        DvachService dvachService = new Retrofit.Builder()
+                .baseUrl("https://2ch.hk")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+                .create(DvachService.class);
+
+        // Здесь должен быть GET_BOARD, но он не работает.
+        dvachService.getRxThread("b", f)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<Board>() {
+                    @Override
+                    public void onNext(Board board) {
+                        recyclerViewq.setAdapter(new ThreadAdapter(ThreadActivity.this, board.getThreads().get(0).getPosts()));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(ThreadActivity.this, "error :(", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Toast.makeText(ThreadActivity.this, "Completed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        /*Call<Board> call = dvachService.getThread("b", f); // Здесь должен быть GET_BOARD, но он не работает.
         call.enqueue(new Callback<Board>() {
             @Override
             public void onResponse(Call<Board> call, Response<Board> response) {
@@ -62,7 +90,6 @@ public class ThreadActivity extends AppCompatActivity {
             public void onFailure(Call<Board> call, Throwable t) {
                 Toast.makeText(ThreadActivity.this, "error :(", Toast.LENGTH_SHORT).show();
             }
-        });
-
+        });*/
     }
 }
