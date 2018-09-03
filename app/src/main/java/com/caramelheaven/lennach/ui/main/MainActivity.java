272cea861f42075ff1a96bbc1f4f3738533f6f69
CaramelHeaven <srg.fominov@gmail.com>
2018-09-03 21:50:48 +0400

    removed welcome screen

---
+++ app/src/main/java/com/caramelheaven/lennach/ui/main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.caramelheaven.lennach.ui.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.caramelheaven.lennach.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
