c9322f74bea9d08360356a83f770d4a3ac7760b0
CaramelHeaven <srg.fominov@gmail.com>
2018-09-30 16:31:36 +0400

    provided menu in bottom appbar

--- app/src/main/java/com/caramelheaven/lennach/presentation/main/MainActivity.java
+++ app/src/main/java/com/caramelheaven/lennach/presentation/main/MainActivity.java
@@ -1,17 +1,26 @@
 package com.caramelheaven.lennach.presentation.main;
 
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.caramelheaven.lennach.R;
 import com.caramelheaven.lennach.presentation.board.BoardFragment;
+import com.caramelheaven.lennach.presentation.main.navigation.BottomNavigationDrawerFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -19,5 +28,10 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container, BoardFragment.newInstance("b"))
                     .commit();
         }
+
+        bottomAppBar.setNavigationOnClickListener(v -> {
+            BottomNavigationDrawerFragment bottomNav = BottomNavigationDrawerFragment.newInstance();
+            bottomNav.show(getSupportFragmentManager(), null);
+        });
     }
 }
