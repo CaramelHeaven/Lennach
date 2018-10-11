1d661cce80235cfd51b4a85c3ec882fde70d3df3
pelev1n <pelevin9911@gmail.com>
2018-10-11 18:16:57 +0300

    Added Poetry Fragment

--- app/src/main/java/com/caramelheaven/lennach/presentation/main/MainActivity.java
+++ app/src/main/java/com/caramelheaven/lennach/presentation/main/MainActivity.java
@@ -1,12 +1,14 @@
 package com.caramelheaven.lennach.presentation.main;
 
 import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.caramelheaven.lennach.R;
 import com.caramelheaven.lennach.presentation.board.BoardFragment;
@@ -15,12 +17,13 @@ import com.caramelheaven.lennach.presentation.main.navigation.BottomNavigationDr
 public class MainActivity extends AppCompatActivity {
 
     private BottomAppBar bottomAppBar;
-
+    private FloatingActionButton createThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomAppBar = findViewById(R.id.bottom_app_bar);
+        createThread = findViewById(R.id.fab_create_thread);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -32,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         bottomAppBar.setNavigationOnClickListener(v -> {
             BottomNavigationDrawerFragment bottomNav = BottomNavigationDrawerFragment.newInstance();
             bottomNav.show(getSupportFragmentManager(), null);
+        });
+
+        createThread.setOnClickListener(v -> {
+            System.out.println("CREATER THREAD ACTIVITY!");
         });
     }
 }
